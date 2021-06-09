6b3a8f4301542950e96ac55eb1d602c33fc9821b
muradisgandar <murad.isgandar@gmail.com>
2021-06-10 00:03:04 +0400

    initial commit

---
+++ src/main/java/com/murad/spvue/SpVueApplication.java
@@ -0,0 +1,13 @@
+package com.murad.spvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpVueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murad/spvue/SpVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murad.spvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpVueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
